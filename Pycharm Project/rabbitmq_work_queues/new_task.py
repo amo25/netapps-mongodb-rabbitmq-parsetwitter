19f3e9d82735583aec424c3db1d1f1f231168e96
Alex O <amo25@vt.edu>
2019-03-29 13:07:19 -0400

    Work queues

---
+++ Pycharm Project/rabbitmq_work_queues/new_task.py
@@ -0,0 +1,19 @@
+import pika
+import sys
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True) #durable means that task_queue won't be lost, even if rabbitmq restarts (todo do I need to clear this at some point? Don't want this sitting around forever. Will .close() destroy it?)
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(
+    exchange='',
+    routing_key='task_queue',
+    body=message,
+    properties=pika.BasicProperties(
+        delivery_mode=2,  # make message persistent
+    ))
+print(" [x] Sent %r" % message)
+connection.close()
