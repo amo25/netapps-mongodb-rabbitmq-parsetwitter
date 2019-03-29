f3193a987db913bd6ca52d367759980e126d956e
Alex O <amo25@vt.edu>
2019-03-28 23:41:58 -0400

    Pycharm project

---
+++ Pycharm Project/rabbitmq_send.py
@@ -0,0 +1,19 @@
+import pika
+
+#establish a connection with the rabbitmq server, connect a broker on localhost
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host='localhost'))
+channel = connection.channel()
+
+#creates a queue to which message will be delivered, queue named hello
+channel.queue_declare(queue='hello')
+
+#send the string "Hello, World" to the hello queue
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print
+" [x] Sent 'Hello World!'"
+
+#close the connection before exiting
+connection.close()
