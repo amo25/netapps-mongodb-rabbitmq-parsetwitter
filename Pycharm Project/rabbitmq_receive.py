b7011ddad3d7a2603a52583ef1b30ebbbe813355
amo25 <amo25@vt.edu>
2019-03-29 01:44:44 -0400

    Fixed rabbitmq receive

--- Pycharm Project/rabbitmq_receive.py
+++ Pycharm Project/rabbitmq_receive.py
@@ -1,23 +1,19 @@
 import pika
 
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
 
 #the callback function to a subscribe to the queue
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue='hello') #Make sure queue exists - queue_declare is idempotent, so we can run it as many times as we like and only one hello queue will be created
-
-#tell rabbitmq to subscribe particular callback function to hello queue
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+#tell rabbitmq to subscribe particular callback function
+channel.basic_consume(
+    queue='hello', on_message_callback=callback, auto_ack=True)
 
 #never ending loop which waits for messages
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
-
-#to list queue's, on the pi run sudo rabbitmqctl list_queues
-
--- Pycharm Project/rabbitmq_send.py
+++ Pycharm Project/rabbitmq_send.py
@@ -12,8 +12,11 @@ channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='Hello World!')
-print
-" [x] Sent 'Hello World!'"
+
+#print("Hello World")
+#print
+#" [x] Sent 'Hello World!'"
+print(" [x] Sent 'Hello World!'")
 
 #close the connection before exiting
 connection.close()
