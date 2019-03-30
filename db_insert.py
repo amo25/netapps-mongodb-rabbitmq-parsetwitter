4e80d73a87d9a425719447ac021bd7b707c9ca0f
amo25 <amo25@vt.edu>
2019-03-30 19:04:18 -0400

    Authorized produce/consume. Working on MongoDB

--- db_insert.py
+++ db_insert.py
@@ -1,8 +1,18 @@
 import pymongo
+#
+#stats = {"net": {"lo": {"rx": 0,"tx": 0},"wlan0":
+#                 {"rx": 708,"tx": 1192},"eth0": {"rx": 0,"tx":
+#                                                 0}},"cpu": 0.2771314211797171}
+#db = pymongo.MongoClient().test
 
-stats = {"net": {"lo": {"rx": 0,"tx": 0},"wlan0":
-                 {"rx": 708,"tx": 1192},"eth0": {"rx": 0,"tx":
-                                                 0}},"cpu": 0.2771314211797171}
-db = pymongo.MongoClient().test
+#db.utilization.insert(stats)
 
-db.utilization.insert(stats)
+Place = "SquiresA"
+Subject = "Rooms"
+Message = "I like the comfortable chairs"
+
+client = pymongo.MongoClient()
+db = client[Place]
+collection = db[Subject]
+post_id = collection.insert_one([Message]).inserted_id
+#post_id
