5a095553a8157d26e5a2d63bdb03510825e46bc3
amo25 <amo25@vt.edu>
2019-03-31 13:39:06 -0400

    Twitter stuff done. Need to do GPIO and captureKeys

--- Structure/capture.py
+++ Structure/capture.py
@@ -7,13 +7,21 @@
 import RPi.GPIO as GPIO
 import time
 import sys
-
-# TODO:
-#import Tweepy                       # for twitter
 import pymongo                      # for nosql
 import json
 import pika                         # for rabbitmq
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import re
 #import captureKeys
+#todo
+consumer_key ="JdiNWuJilkltEyx7dw0pabzTb"
+consumer_secret="fpPBbkOpXc79AlPizGc4XuN5s5GW1HdZr7c7XSXBl4kK91O7RW"
+
+
+access_token="971136871-J1bbxHr2ocolO5zkM3gSrpfAMjK639lSu0hsthH6"
+access_token_secret="nAjbhswbThQ5j8NuPtwcE1OFVpHHXZ6iGjq99LqtdZBgJ"
 
 RED = 21
 GREEN = 22
@@ -26,7 +34,7 @@ def setGPIO():
     GPIO.setup(channels, GPIO.OUT, initial=GPIO.LOW)
 
 # LED STATUS
-def status(number):
+def set_led(number):
     # white = waiting for a command
     if number == 0:
         GPIO.output(RED, GPIO.LOW)
@@ -43,9 +51,9 @@ def status(number):
         GPIO.output(GREEN, GPIO.HIGH)
         GPIO.output(BLUE, GPIO.LOW)
 
-def read_tweet(hashtag):
-    #all your stuff
-    return (original_tweet, Action, Place, Subject, Message)
+#def read_tweet(hashtag):
+#    #all your stuff
+#    return (original_tweet, Action, Place, Subject, Message)
         
         
 #inserts a message to mongoDB
@@ -102,44 +110,120 @@ def consume(host, Place, Subject):
 
 setGPIO()
 #arguments
+#hashtag = ''
 if len(sys.argv) == 5:
     host = sys.argv[2]
     hashtag = sys.argv[4]
 else:
     print("Argument format error. Use: python3 capture.py -s <SERVER_IP> -t \"<HASHTAG>\"")
     
+#def read_tweet(hashtag):
+#    #all your stuff
+#    return (original_tweet, Action, Place, Subject, Message)
+
+    #parse the tweet
+def parse_tweet(original_tweet, hashtag):
+    s = re.sub("\s*"+hashtag+"\s*","",original_tweet)
+    command = s.split(':')[0]
+    command = command.strip() #strip removes whitespace from beginning or end of a character
+    therest = s.split(':')[1]
+    Place = therest.split('+')[0]
+    Place = Place.strip()
+    therest = therest.split('+')[1]
+    if (command == 'p'):
+        Subject = therest.split('\"')[0]
+        Subject = Subject.strip()
+        Message = therest.split('\"')[1]
+        Message.strip()
+    elif (command == 'c'):
+        Subject = therest.strip()
+        Message = None
+    
+    return (command, Place, Subject, Message)
+    
+
+#Twitter stuff
+class StdOutListener(StreamListener):
+    #reads tweets
+    def on_status(self, status):
+        original_tweet=status.text #read the tweet
+        
+        print("[Checkpoint 01 " + str(time.time()) + "] Tweet captured:" + original_tweet)
+        #parse the tweet
+        (Action, Place, Subject, Message) = parse_tweet(original_tweet, hashtag)
+        
+        #Store in mongoDB
+        json_string = db_insert(Action,Place,Subject,Message)
+        print("[Checkpoint 02 " + str(time.time()) + "] Store command in MongoDB instance: " + str(json_string))
+        
+        print("[Checkpoint 03 " + str(time.time()) + "] GPIO LED: ")
+        if (Action == 'p'):
+            print("[Checkpoint 04 " + str(time.time()) + "] Produce, Place=" + Place + ", Subject=" + Subject + ", Message=" + Message)
+            set_led(1)
+            print("[Checkpoint 05 " + str(time.time()) + "]")
+            produce(host, Place, Subject, Message)
+            time.sleep(1)
+            set_led(0)
+            
+        elif (Action == 'c'):
+            print("[Checkpoint 04 " + str(time.time()) + "] Consume, Place=" + Place + ", Subject=" + Subject)
+            set_led(2)
+            print("[Checkpoint 05 " + str(time.time()) + "]")
+            consume(host, Place, Subject)
+            time.sleep(1)
+            set_led(0)
+        
+        return True
+
+    #returns if too many attempts are made to connect to the API stream
+    def on_error(self, status_code):
+        if status_code == 420:
+            print("Too many attempts made to connect to Twitter API stream. Wait a bit")
+            return False
+        
+
+#todo add status(0) somewhere
+if __name__ == '__main__':
+    set_led(0)
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, l)
+    stream.filter(track=[hashtag]) #todo ensure it can handle this
+    
 
 # some loop to continuously check/process tweets
 # /update led/messssage stuff/wait for response
 # print the message command sent
-while (True):
-    status(0) #set LED to white while waiting for command
-    time.sleep(3) #todo remove
-    #block until a tweet is read
-    #parse the tweet
-    Action = "c" #todo pull from tweet
-    Place = "Squires"
-    Subject = "Rooms" #todo allow it to fail gracefully if we attempt to consume from a nonexistant queue?
-    Message = "This room is something"
-    original_tweet = "#ECE4564T19 p:Squires+Rooms \"This room is something\""
-    print("[Checkpoint 01 " + str(time.time()) + "] Tweet captured:" + original_tweet)
-    
-    #Store in mongoDB
-    json_string = db_insert(Action,Place,Subject,Message)
-    print("[Checkpoint 02 " + str(time.time()) + "] Store command in MongoDB instance: " + str(json_string))
-    
-    print("[Checkpoint 03 " + str(time.time()) + "] GPIO LED: ")
-    if (Action == 'p'):
-        print("[Checkpoint 04 " + str(time.time()) + "] Produce, Place=" + Place + ", Subject=" + Subject + ", Message=" + Message)
-        status(1)
-        print("[Checkpoint 05 " + str(time.time()) + "]")
-        produce(host, Place, Subject, Message)
-        
-    elif (Action == 'c'):
-        print("[Checkpoint 04 " + str(time.time()) + "] Consume, Place=" + Place + ", Subject=" + Subject)
-        status(2)
-        print("[Checkpoint 05 " + str(time.time()) + "]")
-        consume(host, Place, Subject)
+#while (True):
+#    status(0) #set LED to white while waiting for command
+#    #time.sleep(3) #todo remove
+#    #block until a tweet is read
+#    #parse the tweet
+#    Action = "c" #todo pull from tweet
+#    Place = "Squires"
+#    Subject = "Rooms" #todo allow it to fail gracefully if we attempt to consume from a nonexistant queue?
+#    Message = "This room is something"
+#    original_tweet = "#ECE4564T19 p:Squires+Rooms \"This room is something\""
+#    print("[Checkpoint 01 " + str(time.time()) + "] Tweet captured:" + original_tweet)
+#    
+#    #Store in mongoDB
+#    json_string = db_insert(Action,Place,Subject,Message)
+#    print("[Checkpoint 02 " + str(time.time()) + "] Store command in MongoDB instance: " + str(json_string))
+#    
+#    print("[Checkpoint 03 " + str(time.time()) + "] GPIO LED: ")
+#    if (Action == 'p'):
+#        print("[Checkpoint 04 " + str(time.time()) + "] Produce, Place=" + Place + ", Subject=" + Subject + ", Message=" + Message)
+#        status(1)
+#        print("[Checkpoint 05 " + str(time.time()) + "]")
+#        produce(host, Place, Subject, Message)
+#        
+#    elif (Action == 'c'):
+#        print("[Checkpoint 04 " + str(time.time()) + "] Consume, Place=" + Place + ", Subject=" + Subject)
+#        status(2)
+#        print("[Checkpoint 05 " + str(time.time()) + "]")
+#        consume(host, Place, Subject)
         
         
     
--- Structure/captureKeys.py
+++ Structure/captureKeys.py
@@ -1,6 +1,16 @@
-#Filename: repository.py
 #Description: capture api/dev key file
 #ECE 4564 - Assignment 2
 #Team 19 : Alexander Orlov, Maura Hartmann, Mark Owen
 
-#TODO: insert twitter key stuffs
+#twitter key stuffs
+
+#print("Beginning")
+
+consumer_key ="JdiNWuJilkltEyx7dw0pabzTb"
+consumer_secret="fpPBbkOpXc79AlPizGc4XuN5s5GW1HdZr7c7XSXBl4kK91O7RW"
+
+
+access_token="971136871-J1bbxHr2ocolO5zkM3gSrpfAMjK639lSu0hsthH6"
+access_token_secret="nAjbhswbThQ5j8NuPtwcE1OFVpHHXZ6iGjq99LqtdZBgJ"
+
+#print("End")
