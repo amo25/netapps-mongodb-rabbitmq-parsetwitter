39243e4aec2e9dc30bd6161dc9ff82dc80a643e2
amo25 <amo25@vt.edu>
2019-03-31 13:39:06 -0400

    Twitter stuff done. Need to do GPIO and captureKeys

--- Structure/captureKeys.py
+++ Structure/captureKeys.py
@@ -1,6 +1,16 @@
-#Filename: repository.py
 #Description: capture api/dev key file
 #ECE 4564 - Assignment 2
 #Team 19 : Alexander Orlov, Maura Hartmann, Mark Owen
 
-#TODO: insert twitter key stuffs
+#twitter key stuffs
+
+#print("Beginning")
+
+consumer_key ="***REMOVED***"
+consumer_secret="***REMOVED***"
+
+
+access_token="***REMOVED***"
+access_token_secret="***REMOVED***"
+
+#print("End")
